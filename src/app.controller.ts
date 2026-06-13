cb2a1368e6244dbc1839947717fc51b760deb6ad
Dinh Thanh <dinhthanh11011@gmail.com>
2026-06-13 19:07:37 +0700

    change health to ping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,19 +2,19 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@ApiTags('health')
+@ApiTags('ping')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get('health')
-  @ApiOperation({ summary: 'Health check endpoint' })
-  @ApiResponse({
-    status: 200,
-    description: 'Service is healthy',
-    schema: { type: 'string', example: 'OK' },
-  })
-  getHealth(): string {
-    return this.appService.getHealth();
-  }
+    @Get('ping')
+    @ApiOperation({ summary: 'Health check endpoint' })
+    @ApiResponse({
+        status: 200,
+        description: 'Service is healthy',
+        schema: { type: 'string', example: 'OK' },
+    })
+    getHealth(): string {
+        return this.appService.getHealth();
+    }
 }
